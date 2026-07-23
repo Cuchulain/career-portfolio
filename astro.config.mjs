5767871752b2eab4513531e5b4c03e53c2389d81
Jan Čejka <posta@jancejka.cz>
2026-07-23 11:21:35 +0200

    Umami analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
+import umami from '@yeskunall/astro-umami';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dev.jancejka.cz',
@@ -14,5 +16,12 @@ export default defineConfig({
     plugins: [tailwindcss()]
   },
 
-  integrations: [icon(), sitemap()]
+  integrations: [
+    icon(),
+    sitemap(),
+    umami({
+        id: "b2ccf8e5-3071-4013-8f80-202fcba0fee5",
+        endpointUrl: "https://analytics.rebma.cz"
+    })
+  ]
 });
